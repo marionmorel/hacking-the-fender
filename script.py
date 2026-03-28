4c82b8c9ca61f1d7314bdab048f8e3b8ecf7dd84
marionmorel <marion.mrl@live.fr>
2026-03-28 16:32:06 +0000

    Tasks 16 - 19

--- script.py
+++ script.py
@@ -43,3 +43,30 @@ with open('boss_message.json', 'w') as boss_message:
 
 # Task 15
     json.dump(boss_message_dict, boss_message)
+
+# Task 16
+with open('new_passwords.csv', 'w') as new_passwords_obj: 
+
+# Task 17
+    slash_null_sig = '''
+     _  _     ___   __  ____             
+    / )( \   / __) /  \(_  _)            
+    ) \/ (  ( (_ \(  O ) )(              
+    \____/   \___/ \__/ (__)             
+    _  _   __    ___  __ _  ____  ____  
+    / )( \ / _\  / __)(  / )(  __)(    \ 
+    ) __ (/    \( (__  )  (  ) _)  ) D ( 
+    \_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+     ___   / ___)(  )   / _\ / ___)/ )( \
+    (___)  \___ \/ (_/\/    \\___ \) __ (
+           (____/\____/\_/\_/(____/\_)(_/
+    __ _  _  _  __    __                
+    (  ( \/ )( \(  )  (  )               
+    /    /) \/ (/ (_/\/ (_/\             
+    \_)__)\____/\____/\____/
+    '''
+
+# Task 18
+# Task 19
+    new_passwords_obj.write(slash_null_sig)
